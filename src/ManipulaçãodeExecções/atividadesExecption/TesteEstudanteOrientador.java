4825b9939cccc7d12e3f20f5f647ac9b32df7428
Murillo Alves <tumusx1@gmail.com>
2021-12-15 23:03:37 -0300

    uploud files Exceptioon

--- src/ManipulaçãodeExecções/atividadesExecption/TesteEstudanteOrientador.java
+++ src/ManipulaçãodeExecções/atividadesExecption/TesteEstudanteOrientador.java
@@ -8,12 +8,16 @@ public class TesteEstudanteOrientador {
         Estudante e1 = new Estudante(45210);
         Estudante e2 = new Estudante(45421);
 
-
+        try {
+        o1.adicionarAluno(e1);
         o1.adicionarAluno(e1);
         e2.adicionarOrientador(o2);
         o1.removerAluno(e1);
+        e2.adicionarOrientador(o1);
 
         o1.imprimirOrientadorEstudante();
         o2.imprimirOrientadorEstudante();
-    }
-}
+    }catch (RuntimeException e){
+        System.out.println(e.getMessage());
+        }
+}}
